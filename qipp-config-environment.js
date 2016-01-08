22790c552c5b6e56d98b0929e3315a2c32bdd1ae
aausstm <aaustm@googlemail.com>
2016-01-08 12:15:51 +0100

    Update qipp-config-environment.js
    Parse domainprefix and subdomains.

--- qipp-config-environment.js
+++ qipp-config-environment.js
@@ -8,12 +8,13 @@
             function (
                 config
             ) {
-                var prefix = (window.location.href.match(/(dev-|staging-).+\..+\..+/) || ['']).pop(),
+                var parts = (window.location.href.match(/(dev-|staging-)?([\w-]+)\..+\..+/)),
                     domain = '.qipp.com',
                     protocol = 'https://';
-                config.api.host = protocol + prefix + 'home' + domain;
+                config.api.host = protocol + (parts[1] || '') + 'home' + domain;
                 config.auth.host =  '';
                 config.auth.prefix =  '/auth/';
+                config.subdomain = parts[2];
             }
         ]);
 
